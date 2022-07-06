a62301988474ac0fbc1d0a56baf4ee0be8b45047
md-ataur <mdataur15@gmail.com>
2022-07-06 23:07:54 +0600

    workflow section done

--- src/components/Workflow/Workflow.tsx
+++ src/components/Workflow/Workflow.tsx
@@ -1,3 +1,12 @@
+import {
+    Accordion,
+    AccordionItem,
+    AccordionItemButton,
+    AccordionItemHeading,
+    // eslint-disable-next-line prettier/prettier
+    AccordionItemPanel
+} from 'react-accessible-accordion';
+import 'react-accessible-accordion/dist/fancy-example.css';
 import underline from '../../images/underline-pricing-plan.png';
 import workflow from '../../images/workflow.png';
 
@@ -11,9 +20,83 @@ const Workflow = () => {
                         <img src={underline} alt="Underline" />
                     </span>
                 </h2>
-                <div>
-                    <div></div>
-                    <div>
+                <div className="accordion-area">
+                    <Accordion className="accordion-left" preExpanded={['a']}>
+                        <AccordionItem uuid="a">
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Defining Goal</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Project analysis, defining project requirements and objectives
+                                    based on clients needs, cost and effort estimation, creating
+                                    value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Product Design</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Project analysis, defining project requirements and objectives
+                                    based on clients needs, cost and effort estimation, creating
+                                    value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Development</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Development Project analysis, defining project requirements and
+                                    objectives based on clients needs, cost and effort estimation,
+                                    creating value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Testing</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Testing Project analysis, defining project requirements and
+                                    objectives based on clients needs, cost and effort estimation,
+                                    creating value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Product Release</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Product Release Project analysis, defining project requirements
+                                    and objectives based on clients needs, cost and effort
+                                    estimation, creating value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                        <AccordionItem>
+                            <AccordionItemHeading>
+                                <AccordionItemButton>Maintenance Support</AccordionItemButton>
+                            </AccordionItemHeading>
+                            <AccordionItemPanel>
+                                <p>
+                                    Maintenance Support Project analysis, defining project
+                                    requirements and objectives based on clients needs, cost and
+                                    effort estimation, creating value and process planning.
+                                </p>
+                            </AccordionItemPanel>
+                        </AccordionItem>
+                    </Accordion>
+
+                    <div className="accordion-right">
                         <img src={workflow} alt="" />
                     </div>
                 </div>
